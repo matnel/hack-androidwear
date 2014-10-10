7e3ce78750c5c64e8e1c07de59b51cd70c0478ac
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-10-10 17:19:43 +0300

    Empty project commit

---
+++ app/src/main/java/li/mante/me/androidhack/Demo.java
@@ -0,0 +1,24 @@
+package li.mante.me.androidhack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class Demo extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
